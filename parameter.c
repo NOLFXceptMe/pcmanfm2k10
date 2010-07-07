baff266f2f35c42cc486d88be6382c0ddff3f7c8
Naveen Kumar Molleti <nerd.naveen@gmail.com>
2010-07-07 12:53:24 +0530

    Modified parameter.c for correct behaviour

--- parameter.c
+++ parameter.c
@@ -11,7 +11,7 @@ void print_gstring(gpointer data, gpointer user_data);
 
 GPtrArray* substitute_parameters(gchar *input_string, FmFileInfoList *file_info_list)
 {
-	gsize i;
+	gsize i, j;
 	gsize len_file_list = fm_list_get_length(file_info_list);
 	GPtrArray *out_string_array = g_ptr_array_new();
 	GString *out_string = g_string_new(NULL);
@@ -21,34 +21,67 @@ GPtrArray* substitute_parameters(gchar *input_string, FmFileInfoList *file_info_
 	g_string_append_len(out_string, input_string, first_pos);
 	GString *g_string_i = NULL;
 	gchar *file_name;
-
-	for(i=0; i<len_file_list; ++i){
-		g_ptr_array_add(out_string_array, g_string_new(out_string->str));
-	}
-	//g_ptr_array_foreach(out_string_array, print_gstring, NULL);
+	gboolean array_is_init = FALSE;
 
 	while((pos = strchr(pos, '%')) != NULL){
 		switch(pos[1]){
 			case 'b':
-				for(i=0; i<len_file_list; ++i){
+				if(array_is_init == FALSE)
+					for(i=0; i<len_file_list; ++i)
+						g_ptr_array_add(out_string_array, g_string_new(out_string->str));
+
+				for(i=0; i<out_string_array->len; ++i){
 					g_string_i = (GString *)g_ptr_array_index(out_string_array, i);
 					file_name = (gchar *)fm_file_info_get_disp_name(fm_list_peek_nth(file_info_list, i));
 					g_string_append(g_string_i, file_name);
+					g_string_append_c(g_string_i, ' ');
 				}
 				break;
 			case 'B':
+				if(array_is_init == FALSE)
+					g_ptr_array_add(out_string_array, g_string_new(out_string->str));
+
+				for(i=0; i<out_string_array->len; ++i){
+					g_string_i = (GString *)g_ptr_array_index(out_string_array, i);
+					for(j=0; j<len_file_list; ++j){
+						file_name = (gchar *)fm_file_info_get_disp_name(fm_list_peek_nth(file_info_list, j));
+						g_string_append(g_string_i, file_name);
+						g_string_append_c(g_string_i, ' ');
+					}
+				}
 				break;
 			case 'c':
+				if(array_is_init == FALSE)
+					for(i=0; i<len_file_list; ++i)
+						g_ptr_array_add(out_string_array, g_string_new(out_string->str));
+
 				break;
 			case 'C':
+				if(array_is_init == FALSE)
+					g_ptr_array_add(out_string_array, g_string_new(out_string->str));
+
 				break;
 			case 'd':
+				if(array_is_init == FALSE)
+					for(i=0; i<len_file_list; ++i)
+						g_ptr_array_add(out_string_array, g_string_new(out_string->str));
+
 				break;
 			case 'D':
+				if(array_is_init == FALSE)
+					g_ptr_array_add(out_string_array, g_string_new(out_string->str));
+
 				break;
 			case 'f':
+				if(array_is_init == FALSE)
+					for(i=0; i<len_file_list; ++i)
+						g_ptr_array_add(out_string_array, g_string_new(out_string->str));
+
 				break;
 			case 'F':
+				if(array_is_init == FALSE)
+					g_ptr_array_add(out_string_array, g_string_new(out_string->str));
+
 				break;
 			case 'h':
 				break;
@@ -59,16 +92,29 @@ GPtrArray* substitute_parameters(gchar *input_string, FmFileInfoList *file_info_
 			case 's':
 				break;
 			case 'u':
+				if(array_is_init == FALSE)
+					for(i=0; i<len_file_list; ++i)
+						g_ptr_array_add(out_string_array, g_string_new(out_string->str));
 				break;
 			case 'U':
+				if(array_is_init == FALSE)
+					g_ptr_array_add(out_string_array, g_string_new(out_string->str));
+
 				break;
 			case 'w':
 				break;
 			case 'W':
 				break;
 			case 'x':
+				if(array_is_init == FALSE)
+					for(i=0; i<len_file_list; ++i)
+						g_ptr_array_add(out_string_array, g_string_new(out_string->str));
+
 				break;
 			case 'X':
+				if(array_is_init == FALSE)
+					g_ptr_array_add(out_string_array, g_string_new(out_string->str));
+
 				break;
 			case '%':
 				break;
@@ -76,13 +122,8 @@ GPtrArray* substitute_parameters(gchar *input_string, FmFileInfoList *file_info_
 				return NULL;
 		}
 
-		/*
-		g_free(temp_string0);
-		temp_string0 = g_strcpy(temp_string1);
-		g_free(temp_string1);
-		*/
-
 		pos += 2;
+		(array_is_init == FALSE)?array_is_init = TRUE:0;
 	}
 	/*
 	out_string = g_strcpy(temp_string0);
